4cb7c7423e1efe7cfa2bc8e59b98060b7590f49c
MisaOgura <misa.ogura01@gmail.com>
2018-02-13 16:30:39 +0000

    Use async/await with aws-sdk

--- app/routers/apiRouter.js
+++ app/routers/apiRouter.js
@@ -16,34 +16,35 @@ const s3 = new AWS.S3()
 
 const apiRouter = Router()
 
-apiRouter.post('/images', (req, res) => {
+apiRouter.post('/images', async (req, res) => {
   const imageData = req.files.file
   const imageFile = fs.readFileSync(imageData.file)
 
-  const params = {
-    Body: imageFile,
-    Bucket: process.env.AWS_S3_BUCKET,
-    Key: imageData.filename
-  }
+  try {
+    const params = {
+      Body: imageFile,
+      Bucket: process.env.AWS_S3_BUCKET,
+      Key: imageData.filename
+    }
+    console.log(params)
 
-  console.log(params)
+    const responseFromS3 = await s3.putObject(params).promise()
+    console.log(responseFromS3)
 
-  s3.putObject(params).promise()
-    .then(data => {
-      console.log(data)
-      const params = {
-        Image: {
-          S3Object: {
-            Bucket: process.env.AWS_S3_BUCKET,
-            Name: imageData.filename
-          }
+    const options = {
+      Image: {
+        S3Object: {
+          Bucket: process.env.AWS_S3_BUCKET,
+          Name: imageData.filename
         }
       }
+    }
+    const responseFromRk = await rekognition.detectText(options).promise()
 
-      rekognition.detectText(params).promise()
-        .then(data => res.status(200).send(data))
-        .catch(err => console.error(err, err.stack))
-    }).catch(err => console.error(err, err.stack))
+    res.status(200).send(responseFromRk)
+  } catch (err) {
+    console.error(err, err.stack)
+  }
 })
 
 export default apiRouter
