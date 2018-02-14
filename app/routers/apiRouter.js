78b0c1469d90d2ee669d45fd2101b704694eb69d
MisaOgura <misa.ogura01@gmail.com>
2018-02-14 13:54:02 +0000

    Implement openCV

--- app/routers/apiRouter.js
+++ app/routers/apiRouter.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import AWS from 'aws-sdk'
 import { Router } from 'express'
+// import cv from 'opencv'
 
 require('dotenv').config()
 
@@ -16,15 +17,56 @@ const s3 = new AWS.S3()
 
 const apiRouter = Router()
 
+// let processedFilePath, processedFileName
+
 apiRouter.post('/images', async (req, res) => {
   const imageData = req.files.file
-  const imageFile = fs.readFileSync(imageData.file)
+  const filePath = imageData.file
+  const fileName = imageData.filename
+
+  // cv.readImage(filePath, (err, img) => {
+  //   if (err) throw err
+  //   const width = img.width()
+  //   const height = img.height()
+  //   if (width < 1 || height < 1) throw new Error('Image has no size')
+  //
+  //   // do some cool stuff with img
+  //   // img.detectObject(cv.CAR_SIDE_CASCADE, {}, (err, objects) => {
+  //   //   if (err) throw err
+  //   //
+  //   //   console.log('Objects: ', objects)
+  //   //
+  //   //   objects.map(obj =>
+  //   //     img.ellipse(obj.x + obj.width / 2, obj.y + obj.height / 2, obj.width / 2, obj.height / 2, [255, 255, 0], 3)
+  //   //   )
+  //   //
+  //   //   img.save(`./public/images/processed_detectObject_${fileName}`)
+  //   //   console.log('Image saved.')
+  //   // })
+  //
+  //   img.convertGrayscale()
+  //   img.gaussianBlur([3, 3])
+  //
+  //   const lowThresh = 200
+  //   const highThresh = 400
+  //
+  //   img.canny(lowThresh, highThresh)
+  //
+  //   // save img
+  //   console.log('Processing...')
+  //   processedFileName = `grayscale_canny-${lowThresh}-${highThresh}_${fileName}`
+  //   processedFilePath = `./public/images/${processedFileName}`
+  //   img.save(processedFilePath)
+  //   console.log(`Processed image saved in ${processedFilePath}`)
+  // })
+
+  const imageBinary = fs.readFileSync(filePath)
 
   try {
     const params = {
-      Body: imageFile,
+      Body: imageBinary,
       Bucket: process.env.AWS_S3_BUCKET,
-      Key: imageData.filename
+      Key: fileName
     }
     await s3.putObject(params).promise()
 
@@ -32,7 +74,7 @@ apiRouter.post('/images', async (req, res) => {
       Image: {
         S3Object: {
           Bucket: process.env.AWS_S3_BUCKET,
-          Name: imageData.filename
+          Name: fileName
         }
       }
     }
